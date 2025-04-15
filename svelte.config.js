ee829cf6559aa7790218e76284bc2174fb9749b9
Yer <a@b.c>
2025-04-15 22:52:11 +0800

    up - up to new version

--- svelte.config.js
+++ svelte.config.js
@@ -3,28 +3,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
-	},
-	compilerOptions: {
-		customElement: true
-	},
-	onwarn: (warning, handler) => { // 关闭警告
-
-		switch (warning.code) {
-			case "a11y-click-events-have-key-events":
-			case "a11y-no-static-element-interactions":
-				return;
-		}
-
-		handler(warning);
 	}
 };
 
